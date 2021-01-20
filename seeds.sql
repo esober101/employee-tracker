0b759c9f177e60780886a0166caa3f7b97ab578b
Eric Ober <ericscottober@gmail.com>
2021-01-19 22:00:08 -0500

    Updated seeds sql

--- seeds.sql
+++ seeds.sql
@@ -5,9 +5,9 @@ VALUES (1, "FOH"),
 (2, "BOH"), 
 (3, "MGMT");
 
-INSERT INTO role (id, title, salary, deparment_id) 
+INSERT INTO role (id, title, salary, department_id) 
 VALUES (1, "Cashier", 30000, 1), 
-(2 "Cook", 30000, 2), 
+(2, "Cook", 30000, 2), 
 (3, "Manager", 40000, 3);
 
 INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
